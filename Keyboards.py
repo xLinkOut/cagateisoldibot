1fe0a517dc23b95440d5acc61615599f056da2ac
LinkOut <luca.cirillo5@gmail.com>
2018-11-29 18:19:20 +0100

    Expiration date keyboard (probably not the best way but better than raw input from the user).

--- Keyboards.py
+++ Keyboards.py
@@ -57,3 +57,39 @@ def buildKeyboardForPayment(users,status):
 # -- Reset Inline Keyboard -- #
 Reset = InlineKeyboardMarkup()
 Reset.add(InlineKeyboardButton('💀 Reset 💀',callback_data='reset'))
+
+# -- Date Inline Keyboard -- #
+DateKeyboard = InlineKeyboardMarkup()
+DateKeyboard.add(
+    InlineKeyboardButton(text='1',callback_data='date_1'),
+    InlineKeyboardButton(text='2',callback_data='date_2'),
+    InlineKeyboardButton(text='3',callback_data='date_3'),
+    InlineKeyboardButton(text='4',callback_data='date_4'),
+    InlineKeyboardButton(text='5',callback_data='date_5'),
+    InlineKeyboardButton(text='6',callback_data='date_6'),
+    InlineKeyboardButton(text='7',callback_data='date_7'),
+    InlineKeyboardButton(text='8',callback_data='date_8'),
+    InlineKeyboardButton(text='9',callback_data='date_9'),
+    InlineKeyboardButton(text='10',callback_data='date_10'),
+    InlineKeyboardButton(text='11',callback_data='date_11'),
+    InlineKeyboardButton(text='12',callback_data='date_12'),
+    InlineKeyboardButton(text='13',callback_data='date_13'),
+    InlineKeyboardButton(text='14',callback_data='date_14'),
+    InlineKeyboardButton(text='15',callback_data='date_15'),
+    InlineKeyboardButton(text='16',callback_data='date_16'),
+    InlineKeyboardButton(text='17',callback_data='date_17'),
+    InlineKeyboardButton(text='18',callback_data='date_18'),
+    InlineKeyboardButton(text='19',callback_data='date_19'),
+    InlineKeyboardButton(text='20',callback_data='date_20'),
+    InlineKeyboardButton(text='21',callback_data='date_21'),
+    InlineKeyboardButton(text='22',callback_data='date_22'),
+    InlineKeyboardButton(text='23',callback_data='date_23'),
+    InlineKeyboardButton(text='24',callback_data='date_24'),
+    InlineKeyboardButton(text='25',callback_data='date_25'),
+    InlineKeyboardButton(text='26',callback_data='date_26'),
+    InlineKeyboardButton(text='27',callback_data='date_27'),
+    InlineKeyboardButton(text='28',callback_data='date_28'),
+    InlineKeyboardButton(text='29',callback_data='date_29'),
+    InlineKeyboardButton(text='30',callback_data='date_30'),
+    InlineKeyboardButton(text='31',callback_data='date_31')
+)
