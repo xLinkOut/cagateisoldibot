7ae2feda842aaa39cd78657cbabc3b4298ce2048
LinkOut <luca.cirillo5@gmail.com>
2018-11-29 11:51:52 +0100

    Added a first listener for 'new_chat_member' and 'group_created', when bot send its start message and create a record for the group into database.

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import telebot, logging, os
-import Settings, Statements
+import Settings, Statements, Keyboards
 
 # Check for database file
 if not (os.path.isfile(Settings.DatabaseFile)):
@@ -17,6 +17,17 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 # Create bot obj with token in settings file
 bot = telebot.TeleBot(Settings.API_TOKEN)
+
+# Bot added in a group (also during group's creation)
+@bot.message_handler(content_types=['group_chat_created','new_chat_members'])
+def added_in_a_group(message):
+    # If the member added is the bot itself
+    if int(message.new_chat_member.id) == int(bot.get_me().id):
+        # Send start message, with start keyboard and save the message id into database
+        msg_id = bot.send_message(message.chat.id,Statements.IT.Start.replace('$$',message.from_user.first_name),reply_markup=Keyboards.Start,parse_mode='markdown').message_id
+        # Create an half-empty record for the new group
+        Utils.executeQuery("INSERT INTO GROUPS(GROUP_ID,START_MSG_ID,ADMIN_ID) VALUES(?,?,?)",[message.chat.id,msg_id,message.from_user.id])
+
 
 # Put bot in polling state, waiting for incoming message
 bot.polling()
