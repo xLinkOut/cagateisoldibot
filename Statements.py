10ef60193ba05c2420b67d43e629bcd8f3f9013b
LinkOut <luca.cirillo5@gmail.com>
2018-11-29 18:24:36 +0100

    Statements in Italian language.

--- Statements.py
+++ Statements.py
@@ -8,7 +8,23 @@ IT = Language()
 
 # --- IT Statements --- #
     # Text message
-    
+IT.Start = "Hey, *Netflixers!* 🤓\nCon chi di voi $$ condivide l'account? 👇"
+IT.UseThis = "⚠️ Una procedura di configurazione è già attiva, usa questo messaggio. ☝️\n\nSe è stato accidentalmente eliminato, ricomincia la configurazione premendo qui 👇"
+IT.ConfirmList = "Confermi la lista di partecipanti?"
+IT.Schedule = "📆 In che *giorno del mese* viene rinnovato il tuo account?"
+IT.ConfirmSchedule = "Confermi che il tuo account Netflix viene rinnovato il giorno *$$*?"
+IT.Done = "Finito! Quindi...\n\n🎬 I Netflixers sono:\n*$$*\n📆 L'account si rinnova il *$$* di ogni mese.\n🔔 Le notifiche sono attive.\n\nBuona visione! 😎"
+IT.TimeToPay = "📢 Un altro mese è passato, per poter continuare a guardare tutte le serie TV su Netflix è necessario pagare la propria parte.\n\nSono *$$€ a testa*. 💸"
+IT.EveryonePaid = "Tutti hanno pagato la propria parte! Prossimo appuntamento il $$.\n\n*Buon Netflix!* 😎"
+IT.AlreadyConfigured = "Le impostazioni per questo gruppo sono già state confermate oppure una procedura di configurazione è in corso. Se hai bisogno di ripartire da zero, premi sotto"
+IT.NewConfig = "Per riconfigurare il gruppo, premi 👉 /start"
+IT.ConfirmReset = "Sicuro di voler resettare le impostazioni? *Questa azione è irreversibile.*"
     # Callback query
-    
-    # Private chat
+IT.MaxReached = "⚠️ E' già stato raggiunto il numero massimo di partecipanti (4)!"
+IT.AlreadySigned = "Sei già un Netflixers! 😎\nSe hai premuto per sbaglio oppure hai cambiato idea, puoi toglierti dalla lista facendo un tap sul tuo nome."
+IT.AtLeastOneUser = "⚠️ E' necessario almeno un partecipante per andare avanti."
+IT.NotAdmin = "Non sei tu l'amministratore, mi dispiace. 🙁"
+IT.AlreadyPayed = '$$ ha già pagato! 💰'
+IT.Resetted = 'Le impostazioni del gruppo sono state resettate con successo! ✅'
+    # Private Chat Statements
+IT.AddMeInAGroup = "⚠️ La chat privata **non** è attualmente disponibile! Lo sarà in futuro, per permettere la gestione dei propri gruppi Netflix. ⚙️\nPer ora, prova ad aggiungermi in un gruppo!"
