3b02b6d75f29eab57313465bb22369cdc409cfea
LinkOut <luca.cirillo5@gmail.com>
2018-11-29 17:53:10 +0100

    Defined handlers and functions for general yes/no callback.

--- Main.py
+++ Main.py
@@ -51,7 +51,7 @@ def start(message):
         bot.send_message(message.chat.id,Statements.IT.AddMeInAGroup,parse_mode='markdown')
 
 # User that tap on 'I Use Netflix' button and join the bot
-@bot.callback_query_handler(func=lambda call: call.data == 'iousonetflix')
+@bot.callback_query_handler(func=lambda call: call.data == 'iusenetflix')
 def addMember(call):
     # Max reached 
     if Utils.numeroPartecipanti(call.message.chat.id) == 4:
@@ -73,13 +73,100 @@ def addMember(call):
 @bot.callback_query_handler(func=lambda call: 'remove_' in call.data)
 def removeUser(call):    
     # Delete user from db
-    Utils.executeQuery("DELETE FROM PARTECIPANTI WHERE CHAT_ID=? AND GROUP_ID=?",[call.from_user.id,call.message.chat.id])
+    Utils.executeQuery("DELETE FROM USERS WHERE CHAT_ID=? AND GROUP_ID=?",[call.from_user.id,call.message.chat.id])
     # Decrement counter in GROUPS table
-    Utils.executeQuery("UPDATE GROUPS SET NUMERO_PARTECIPANTI=NUMERO_PARTECIPANTI - 1 WHERE GROUP_ID=?",[call.message.chat.id])
+    Utils.executeQuery("UPDATE GROUPS SET NETFLIXERS=NETFLIXERS - 1 WHERE GROUP_ID=?",[call.message.chat.id])
     # Create an updated keyboard
     updatedKeyboard = Keyboards.buildKeyboardForUser(Utils.listaPartecipanti(call.message.chat.id))
     # Edit keyboard markup in the same message
     bot.edit_message_reply_markup(chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=updatedKeyboard)
+
+# Confirm netflixers's list
+@bot.callback_query_handler(func=lambda call: call.data == 'hereweare')
+def hereweare(call):
+    # If user is not admin
+    if call.from_user.id != Utils.getAdminID(call.message.chat.id):
+        bot.answer_callback_query(call.id,Statements.IT.NotAdmin,show_alert=True,cache_time=10)
+    else:
+        # If there aren't netflixers
+        if Utils.numeroPartecipanti(call.message.chat.id) == 0:
+            bot.answer_callback_query(call.id,Statements.IT.AtLeastOneUser,show_alert=True)
+        else:
+            netflixers = ''
+            # Create an updated list of netflixers
+            for index, user in enumerate(Utils.listaPartecipanti(call.message.chat.id)):
+                netflixers += "{} {}\n".format(Keyboards.Numbers[index],user)
+            # Update the same message with a new list 
+            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="{}\n\n*{}*".format(Statements.IT.ConfirmList,netflixers),reply_markup=Keyboards.Confirm,parse_mode='markdown')
+
+# Handler for general 'yes' callback
+@bot.callback_query_handler(func=lambda call: call.data == 'yes')
+def yes(call):
+    # If user is not admin
+    if call.from_user.id != Utils.getAdminID(call.message.chat.id):
+        bot.answer_callback_query(call.id,Statements.IT.NotAdmin,show_alert=True,cache_time=10)
+    else:
+        # Netflixers's list confirmation 
+        if call.message.text[:34] == Statements.IT.ConfirmList:
+            bot.edit_message_text(Statements.IT.Schedule,call.message.chat.id,call.message.message_id,reply_markup=Keyboards.DateKeyboard,parse_mode='markdown')
+        # Expiration's date confirmation
+        elif call.message.text[:-4] == Statements.IT.ConfirmSchedule[:-6]:
+            # Save expiration date in a var
+            Expiration = ''.join(i for i in call.message.text if i.isdigit())
+            # Update expiration date into db            
+            Utils.executeQuery("UPDATE GROUPS SET EXPIRATION=? WHERE GROUP_ID=?",[Expiration,call.message.chat.id])
+            netflixers = ''
+            # Create an updated list
+            for index, user in enumerate(Utils.listaPartecipanti(call.message.chat.id)):
+                netflixers += "{} {}\n".format(Keyboards.Numbers[index],user)
+            # Edit message text with new list
+            bot.edit_message_text(Statements.IT.Done.replace('$$',netflixers,1).replace('$$',Expiration),call.message.chat.id,call.message.message_id,reply_markup={},parse_mode='markdown')
+            # Edit START_MSG_ID into db, set to -1 that mean 'group already configured'
+            Utils.executeQuery("UPDATE GROUPS SET START_MSG_ID=-1 WHERE GROUP_ID=?",[call.message.chat.id])
+            # Add a new scheduled job for the group, on expiration day of every month at 08:00 AM
+            job = scheduler.add_job(paymentNotify,CronTrigger('*','*',int(Expiration),hour=8,minute=00),[call.message.chat.id])
+            # Append job object in jobScheduledList 
+            jobScheduledList.append(job)
+            # Save trigger into db
+            Utils.saveTrigger(job.id,call.message.chat.id,Expiration)
+        # Reset confiration
+        elif call.message.text == Statements.IT.ConfirmReset.replace('*','',2):
+            # Delete information from db, PAYMENTS and USERS tables
+            Utils.executeQuery("DELETE FROM PAYMENTS WHERE GROUP_ID=? AND EXPIRATION=?",[call.message.chat.id,Utils.getSchedule(call.message.chat.id)])
+            Utils.executeQuery("DELETE FROM USERS WHERE GROUP_ID=?",[call.message.chat.id])
+            
+            # Get trigger ID from db
+            triggerID = Utils.getTriggerID(call.message.chat.id)
+            # Remove trigger from scheduled job
+            for index,job in enumerate(jobScheduledList):
+                if job.id == triggerID:
+                    jobScheduledList.pop(index)
+                    break
+            # Delete information from db, TRIGGER and GROUPS tables
+            Utils.executeQuery("DELETE FROM TRIGGER WHERE ID=?",[triggerID])
+            Utils.executeQuery("DELETE FROM GROUPS WHERE GROUP_ID=?",[call.message.chat.id])
+
+            # Edit message with NewConfig statements
+            bot.edit_message_text(Statements.IT.NewConfig,call.message.chat.id,call.message.message_id,reply_markup={},parse_mode='markdown')
+            # Answer with a sucess message
+            bot.answer_callback_query(call.id,Statements.IT.Resetted,show_alert=True,cache_time=10)
+
+# Handler for general 'no' callback
+@bot.callback_query_handler(func=lambda call: call.data == 'no')
+def no(call):
+    # If user is not admin
+    if call.from_user.id != Utils.getAdminID(call.message.chat.id):
+        bot.answer_callback_query(call.id,Statements.IT.NotAdmin,show_alert=True,cache_time=10)
+    else:
+        # If callback is coming from list's confirmation message
+        if call.message.text[:34] == Statements.IT.ConfirmList:
+            bot.edit_message_text(Statements.IT.Start.replace('$$',call.from_user.first_name),call.message.chat.id,call.message.message_id,reply_markup=Keyboards.buildKeyboardForUser(Utils.listaPartecipanti(call.message.chat.id)),parse_mode='markdown')
+        # If callback is coming from expiration's confirmation message
+        elif call.message.text[:-4] == Statements.IT.ConfirmSchedule[:-6]:
+            bot.edit_message_text(Statements.IT.Schedule,call.message.chat.id,call.message.message_id,reply_markup=Keyboards.DateKeyboard,parse_mode='markdown')
+        # If callback is coming from reset's confirmation message        
+        elif call.message.text == Statements.IT.ConfirmReset.replace('*','',2):
+            bot.edit_message_text(Statements.IT.AlreadyConfigured,call.message.chat.id,call.message.message_id,reply_markup=Keyboards.Reset,parse_mode='markdown')
 
 
 # Put bot in polling state, waiting for incoming message
