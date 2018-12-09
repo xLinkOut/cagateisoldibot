edbcca046d57bc188b2608500a26e57cd2c6ffe4
LinkOut <luca.cirillo5@gmail.com>
2018-12-09 15:26:33 +0100

    Added CHAT_ID to PAYMENTS table and fixed primary key for better data consistency.

--- CreateDB.py
+++ CreateDB.py
@@ -27,12 +27,12 @@ Cursor.execute("CREATE TABLE GROUPS ("
                 ")")
 
 # Create a table named USERS that contain information about each user for each group
-# +------------+-----------+--------------------+--------------------------+
-# | GROUP_ID   | INT(50)   | NOT NULL           | Telegram group's ID      |
-# | CHAT_ID    | INT(50)   | PRIMARY KEY        | Telegram user's ID       |
-# | USERNAME   | TEXT(255) | DEFAULT NULL       | User's Telegram username |
-# | FIRST_NAME | TEXT(255) | DEFAULT NULL       | User's first name        |
-# +------------+-----------+--------------------+--------------------------+
+# +------------+-----------+----------------------+--------------------------+
+# | GROUP_ID   | INT(50)   | NOT NULL PRIMARY KEY | Telegram group's ID      |
+# | CHAT_ID    | INT(50)   | NOT NULL PRIMARY KEY | Telegram user's ID       |
+# | USERNAME   | TEXT(255) | DEFAULT NULL         | User's Telegram username |
+# | FIRST_NAME | TEXT(255) | DEFAULT NULL         | User's first name        |
+# +------------+-----------+----------------------+--------------------------+
 Cursor.execute("CREATE TABLE USERS ("
                  "GROUP_ID INT(50) NOT NULL,"
                  "CHAT_ID INT(50) NOT NULL,"
@@ -45,15 +45,17 @@ Cursor.execute("CREATE TABLE USERS ("
 # +------------+-----------+----------------------+----------------------------------------------------------------+
 # | EXPIRATION | DATE      | NOT NULL PRIMARY KEY | Subscription's expiration date                                 |
 # | GROUP_ID   | INT(50)   | NOT NULL PRIMARY KEY | Telegram group's ID                                            |
+# | CHAT_ID    | INT(50)   | NOT NULL PRIMARY KEY | Telegram user's ID                                             |
 # | FIRST_NAME | TEXT(255) | DEFAULT NULL         | User's first name                                              |
 # | STATUS     | INT(1)    | NOT NULL DEFAULT 0   | Payment status: 0=Not payed; 1=Payed; -1=Wait for confirmation |
 # +------------+-----------+----------------------+----------------------------------------------------------------+
 Cursor.execute("CREATE TABLE PAYMENTS("
                  "EXPIRATION DATE NOT NULL,"
                  "GROUP_ID INT(50) NOT NULL,"
+                 "CHAT_ID INT(50) NOT NULL,"
                  "FIRST_NAME TEXT(255) NOT NULL,"
                  "STATUS INT(1) NOT NULL DEFAULT 0,"
-                 "PRIMARY KEY (EXPIRATION,FIRST_NAME)"
+                 "PRIMARY KEY (EXPIRATION,GROUP_ID,CHAT_ID)"
                 ")")
 
 # Create a table named PAYMENTS that contain information about payment notification's trigger
